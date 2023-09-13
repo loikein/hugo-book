55b70c085f7e2c08ade757e0e6b3406730cd9281
loikein <wanleiqiong@gmail.com>
2023-09-13 18:01:55 +0100

    js: update three files; style: add some custom code; layout: update pdf again

--- assets/js/code-linebreak.js
+++ assets/js/code-linebreak.js
@@ -1,17 +1,12 @@
-(function codeLineBreak() {
-  // https://stackoverflow.com/a/29497876
-  // this function considers mainly long urls and inline python and latex code.
-  var i, text, code, codes = document.getElementsByTagName('code');
-  for (i = 0; i < codes.length;) {
-    code = codes[i];
-    if (code.parentNode.tagName !== 'PRE' && code.childElementCount === 0) {
-      // add line break before
-      code.innerHTML = code.innerHTML.replace(/(?<!^|\.|{|\s)(\.|\(|{|\\|@)/g, '<wbr />\$1');
-      // add line break after
-      code.innerHTML = code.innerHTML.replace(/(,|})(?!$|}|\s)/g, '\$1<wbr />');
-      // add line breaks around
-      code.innerHTML = code.innerHTML.replace(/(?<!^|-|\.|\s)(_|-|=|\/)(?!$|\s)/g, '<wbr />\$1<wbr />');
-    }
-    i++;
-  }
-})();
+// https://stackoverflow.com/a/29497876
+// add line break opportunities in long urls and inline code (mainly python/latex).
+[...document.getElementsByTagName('code')].forEach(code => {
+  // skip <pre> tags and <code> that has children or the nolatex class
+  if (code.parentNode.tagName === 'PRE' || code.childElementCount > 0 || code.classList.contains('nolatex')) return;
+  // add line break before
+  code.innerHTML = code.innerHTML.replace(/(?<!^|\.|{|\s)(\.|\(|{|\\|@)/g, '<wbr />\$1');
+  // add line break after
+  code.innerHTML = code.innerHTML.replace(/(,|})(?!$|}|\s)/g, '\$1<wbr />');
+  // add line breaks around
+  code.innerHTML = code.innerHTML.replace(/(?<!^|-|\.|\s)(_|-|=|\/)(?!$|\s)/g, '<wbr />\$1<wbr />');
+});
--- assets/js/latex-fix.js
+++ assets/js/latex-fix.js
@@ -1,21 +1,17 @@
-(function latexFix() {
-  // https://yihui.org/en/2018/07/latex-math-markdown/
-  var i, text, code, codes = document.getElementsByTagName('code');
-  for (i = 0; i < codes.length;) {
-    code = codes[i];
-    if (code.parentNode.tagName !== 'PRE' && code.childElementCount === 0 && !(code.classList.contains("nolatex")) ) {
-      text = code.textContent;
-      if (/^\$[^$]/.test(text) && /[^$]\$$/.test(text)) {
-        text = text.replace(/^\$/, '\\(').replace(/\$$/, '\\)');
-        code.textContent = text;
-      }
-      if (/^\\\((.|\s)+\\\)$/.test(text) || /^\\\[(.|\s)+\\\]$/.test(text) ||
-          /^\$(.|\s)+\$$/.test(text) ||
-          /^\\begin\{([^}]+)\}(.|\s)+\\end\{[^}]+\}$/.test(text)) {
-        code.outerHTML = code.innerHTML;  // remove <code></code>
-        continue;
-      }
-    }
-    i++;
-  };
-})();
+// https://github.com/yihui/misc.js/blob/main/js/math-code.js v1.3.15
+// <code>$math$</code> to \(math\), and <code>$$math$$</code> to $$math$$:
+// https://yihui.org/en/2018/07/latex-math-markdown/
+[...document.getElementsByTagName('code')].forEach(code => {
+  // skip <pre> tags and <code> that has children or the nolatex class
+  if (code.parentNode.tagName === 'PRE' || code.childElementCount > 0 || code.classList.contains('nolatex')) return;
+  let text = code.textContent;
+  if (/^\$[^$]/.test(text) && /[^$]\$$/.test(text)) {
+    text = text.replace(/^\$/, '\\(').replace(/\$$/, '\\)');
+    code.textContent = text;
+  }
+  if (/^\\\((.|\s)+\\\)$/.test(text) || /^\\\[(.|\s)+\\\]$/.test(text) ||
+      /^\$(.|\s)+\$$/.test(text) ||
+      /^\\begin\{([^}]+)\}(.|\s)+\\end\{[^}]+\}$/.test(text)) {
+    code.outerHTML = code.innerHTML;  // remove <code></code>
+  }
+});
